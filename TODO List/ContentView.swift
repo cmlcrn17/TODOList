29f6dea988518d1a3100a3f57f58f4c412f33dd6
Ceren <cemilecerenerdem@gmail.com>
2020-11-01 20:36:32 +0300

    Initial Commit

---
+++ TODO List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO List
+//
+//  Created by Ceren TAŞSIN on 1.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TODO List/TODO_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_ListApp.swift
+//  TODO List
+//
+//  Created by Ceren TAŞSIN on 1.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TODO_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
